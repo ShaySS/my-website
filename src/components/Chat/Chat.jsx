62bd1badc14823cf84529c7629d55e3fa990f21c
Shay Siddiqui <shasiddiqui@deloitte.ca>
2020-12-26 14:35:01 -0700

    CSS grid implemented, aligned items properly

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Chat.css";
+
+export default function Chat({ content, left, right, className }) {
+  return (
+    <div
+      className={`bubble ${className} ${
+        left ? "bubble--left" : "bubble--right"
+      }`}
+    >
+      <p className={left ? "colorLeft" : "colorRight"}>{content}</p>
+    </div>
+  );
+}
--- src/pages/WhoIam/WhoIam.jsx
+++ src/pages/WhoIam/WhoIam.jsx
@@ -1,5 +1,62 @@
 import React from "react";
+import "./WhoIam.css";
+import Chat from "../../components/Chat/Chat";
+import { ReactComponent as Avatar } from "../../assets/female_avatar.svg";
+import { ReactComponent as Me } from "../../assets/chatMe.svg";
 
 export default function whoIam() {
-  return <div>whoIam</div>;
+  return (
+    <>
+      <div className="grid-container">
+        <div className="pic">picarea</div>
+        <div className="text">textarea</div>
+        <div className="avatar">
+          <Avatar className="avatar1" />
+          <Avatar className="avatar2" />
+          <Avatar className="avatar3" />
+        </div>
+        <div className="chatleft">
+          <Chat left content="so, what do you do?" className="l1" />
+          <Chat left content="where do you work?" className="l2" />
+          <Chat
+            left
+            content="anything else you'd like to share?"
+            className="l3"
+          />
+        </div>
+        <div className="chatright">
+          <Chat
+            right
+            content="im an engineer who deploys fullstack applications for the web!"
+            className="r1"
+          />
+          <Chat
+            right
+            content="I’m well versed in React & Node, see my resume for more!"
+            className="r2"
+          />
+          <Chat
+            right
+            content="i’ve been a developer with Deloitte for a little over a year now working in their digital practice"
+            className="r3"
+          />
+          <Chat
+            right
+            content="i’m great at learning independently, new, unfamiliar technologies excite me!"
+            className="r4"
+          />
+          <Chat
+            right
+            content="oh, and i’m a monster at ping-pong. "
+            className="r5"
+          />
+        </div>
+        <div className="myAvatar">
+          <Me className="myAvatar1" />
+          <Me className="myAvatar2" />
+          <Me className="myAvatar3" />
+        </div>
+      </div>
+    </>
+  );
 }
