a704d64f0c2a7e2bdcffa7166066b025c9597b2c
Shay Siddiqui <shasiddiqui@deloitte.ca>
2021-05-03 21:24:32 -0700

    cleaning up infocard

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -5,13 +5,10 @@ export default function infoCard({ cardStyle, heading, body }) {
   return (
     <>
       <div className={styles.container}>
-        <div
-          className={`${styles.heading} 
-            ${styles[cardStyle + "HeadingBackground"]}`}
-        >
+        <div className={styles[cardStyle + "HeadingBackground"]}>
           <p className={`${styles[cardStyle + "HeadingText"]}`}>{heading}</p>
         </div>
-        <div className={`${styles.body} ${styles[cardStyle + "BodyBackground"]}`}>
+        <div className={styles[cardStyle + "BodyBackground"]}>
           <p
             className={`${styles[cardStyle + "BodyText"]}`}
             dangerouslySetInnerHTML={{ __html: body }}
